f05f30b3a122226c677d1f007348660edd685738
kamilproszek <115663533+kamilproszek@users.noreply.github.com>
2022-11-10 04:28:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 let apiKey = "a04d0b88e9d32a0dd9ca3a95b7c3c4e9";
 
 let data;
+var bacground;
+
+var city = document.getElementById("city");
+var temp = document.getElementById("temp");
+var humidity = document.getElementById("humidity");
+var wind = document.getElementById("wind");
+var description = document.getElementById("description");
+var weather_icon = document.querySelector(".weather_icon");
+var main_card = document.getElementById("main-card");
 
 let getWeatherData = (city) => {
   console.log("it works!s");
@@ -19,20 +28,26 @@ let getCity = () => {
   return document.getElementById("search-bar").value;
 };
 
+let changeBacground = () => {
+  main_card.style.background = "url('backgrounds/background.jpg')";
+};
+
+let deleteMainPage = () => {
+  document.getElementById("enter-city").style.display = "none";
+};
+
 let showValue = () => {
+  deleteMainPage();
+  changeBacground();
   getWeatherData(getCity()).then((data) => {
     //podglad w konsoli
     console.log(data);
-
-    document.getElementById("city").innerText = data.name;
-    document.getElementById("temp").innerText = data.main.temp + " °C";
-    document.getElementById("humidity").innerText =
-      "Humidity: " + data.main.humidity + " %";
-    document.getElementById("wind").innerText =
-      "Wind speed: " + data.wind.speed + " km/h";
-    document.getElementById("description").innerText =
-      data.weather[0].description;
-    document.querySelector(".weather_icon").src =
+    city.innerText = data.name + ", " + data.sys.country;
+    temp.innerText = data.main.temp + " °C";
+    humidity.innerText = "Humidity: " + data.main.humidity + " %";
+    wind.innerText = "Wind speed: " + data.wind.speed + " km/h";
+    description.innerText = data.weather[0].description;
+    weather_icon.src =
       "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
   });
 };
